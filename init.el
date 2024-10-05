bc093e78f05d27d756064449bfa71362a8cd54a7
ayako <ayako.gfx@gmail.com>
2024-10-05 18:43:59 +0530

    first commit

---
+++ init.el
@@ -0,0 +1,29 @@
+(require 'package)
+(setq package-enable-at-startup nil)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("gnu" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(treemacs-icons-dired dired doom-modeline compat vertico doom-themes)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(vundo-highlight ((t (:foreground "#FFFF00"))))
+ '(vundo-node ((t (:foreground "#808080"))))
+ '(vundo-stem ((t (:foreground "#808080")))))
