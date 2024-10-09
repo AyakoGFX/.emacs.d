5ee3179169cc69e5ba26985885cad8e35fbc6267
ayako <ayako.gfx@gmail.com>
2024-10-10 01:36:15 +0530

    added org roam ui

--- config.el
+++ config.el
@@ -648,6 +648,25 @@
    ;; Add more levels and colors as needed
    )
 
+(use-package simple-httpd
+  :ensure t)
+
+(use-package websocket
+  :ensure t)
+
+(use-package org-roam-ui
+  :ensure t
+  :after org-roam
+  ;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
+  ;;         a hookable mode anymore, you're advised to pick something yourself
+  ;;         if you don't care about startup time, use
+  ;;  :hook (after-init . org-roam-ui-mode)
+  :config
+  (setq org-roam-ui-sync-theme t
+	org-roam-ui-follow t
+	org-roam-ui-update-on-save t
+	org-roam-ui-open-on-start t))
+
 (use-package jinx  
     :ensure t
     :hook (emacs-startup . global-jinx-mode)
