7826cf013fd80f6bb6d51f6cf7d2ed54d3f1e296
ayako <ayako.gfx@gmail.com>
2026-08-02 01:17:45 +0530

    doom-themes added

---
+++ modules/themes.el
@@ -0,0 +1,11 @@
+(use-package doom-themes
+  :ensure t
+  :custom
+  ;; Global settings (defaults)
+  (doom-themes-enable-bold t)   ; if nil, bold is universally disabled
+  (doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  :config
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
