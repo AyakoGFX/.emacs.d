f7c6b56be24d5dde681d457070d29e7a9c3caff9
Ayako <ayako.gfx@gmail.com>
2026-08-31 00:56:23 +0530

    Orgnized themes and font in one file
    modules/themes-fonts.el

---
+++ modules/themes-fonts.el
@@ -0,0 +1,68 @@
+;; -*- lexical-binding: t; -*-
+(use-package noctalia-theme
+  :ensure nil
+  :no-require t
+  :init
+  (add-to-list 'custom-theme-load-path (expand-file-name "themes/" user-emacs-directory))
+  (load-theme 'noctalia t))
+
+(use-package doom-themes
+  :ensure t
+  :custom
+  (doom-themes-enable-bold t)   ; if nil, bold is universally disabled
+  (doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  :config
+  (doom-themes-visual-bell-config)
+  (doom-themes-org-config))
+
+
+(use-package ef-themes
+  :ensure t
+  :init
+  (ef-themes-take-over-modus-themes-mode 1)
+  :bind
+  ;; (("<f5>" . modus-themes-rotate)
+  ;; ("C-<f5>" . modus-themes-select)
+  ;; ("M-<f5>" . modus-themes-load-random))
+  :config
+  (setq modus-themes-mixed-fonts t)
+  (setq modus-themes-italic-constructs t))
+
+
+;; https://github.com/emacsmirror/mixed-pitch.git
+(use-package mixed-pitch
+  :ensure t
+  :hook
+  (text-mode . mixed-pitch-mode))
+
+(use-package fontaine
+  :ensure t
+  :config
+  (setq fontaine-presets
+        '((regular
+           :default-family "Aporetic Mono"
+           :default-weight regular
+           :default-height 200
+           :fixed-pitch-family "Aporetic Mono"
+           :fixed-pitch-height 1.0
+           :variable-pitch-family "Aporetic Serif"
+           :variable-pitch-height 1.0
+           :bold-family "Aporetic Mono"
+           :bold-weight bold
+           :italic-family "Aporetic Mono"
+           :italic-slant italic)
+          (large
+           :default-family "Aporetic Mono"
+           :default-height 300)
+          (t
+           :default-family "Aporetic Mono"
+           :default-height 110)))
+
+  (fontaine-set-preset 'regular)
+  (fontaine-mode 1))
+
+;; fix sum themes line-number
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (set-face-attribute 'line-number nil :inherit 'default)
+            (set-face-attribute 'line-number-current-line nil :inherit 'default)))
