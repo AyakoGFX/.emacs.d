3d0da0d2ea976b4a6fdd1b61b0e8e5a3c05e1dbd
kishore <ayako.gfx@gmail.com>
2026-09-17 05:15:05 +0530

    removed SpeedBar advice the bug have been fixed
    https://www.reddit.com/r/emacs/comments/1vy7w8r/comment/p5uu81a/?screen_view_count=2&ext-referrer=DIRECT
    
    https://debbugs.gnu.org/cgi/bugreport.cgi?msg=8;filename=0001-speedbar-mode-must-be-enabled-in-the-speedbar-window.patch;bug=81699;att=1

--- modules/jon-tools.el
+++ modules/jon-tools.el
@@ -138,17 +138,7 @@
   :commands (speedbar speedbar-frame)
   :config
   (setq speedbar-prefer-window t)
-  (setq speedbar-use-images nil)
-  ;; https://www.reddit.com/r/emacs/comments/1vy7w8r/comment/p5uu81a/?screen_view_count=2&ext-referrer=DIRECT
-  ;; https://debbugs.gnu.org/cgi/bugreport.cgi?msg=8;filename=0001-speedbar-mode-must-be-enabled-in-the-speedbar-window.patch;bug=81699;att=1
-  (defun my/speedbar-window-mode-ensure-major-mode (&rest _)
-    (unless (buffer-live-p speedbar-buffer)
-      (setq speedbar-buffer (get-buffer-create speedbar--buffer-name)))
-    (with-current-buffer speedbar-buffer
-      (unless (derived-mode-p 'speedbar-mode)
-        (speedbar-mode))))
-
-  (advice-add 'speedbar-window-mode :before #'my/speedbar-window-mode-ensure-major-mode))
+  (setq speedbar-use-images nil))
 
 (use-package ultra-scroll
   :ensure t
